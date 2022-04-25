7a57b30aeb06bc7ad74ed6c3c8ebe95e09b9d195
AleCheGH <chekushkinSasha@gmail.com>
2022-04-25 21:08:55 +0200

    added localizations

---
+++ GitSearchExtra/GitSearchExtra/Localization/Localization.swift
@@ -0,0 +1,37 @@
+//
+//  Localization.swift
+//  GitSearchExtra
+//
+//  Created by Aliaksandr Chekushkin on 4/25/22.
+//
+
+import Foundation
+
+struct Localization {
+    
+    struct Label {
+        static let search = NSLocalizedString("search_label", comment: "")
+        static let repository = NSLocalizedString("repository_label", comment: "")
+        static let repoTitle = NSLocalizedString("repo_title_lable", comment: "")
+        static let commitsHistory = NSLocalizedString("commits_history_label", comment: "")
+    }
+    
+    struct Button {
+        static let shareRepo = NSLocalizedString("share_repo_button", comment: "")
+        static let back = NSLocalizedString("back_button", comment: "")
+        static let viewOnline = NSLocalizedString("view_online_button", comment: "")
+        static let open = NSLocalizedString("open", comment: "")
+        static let cancel = NSLocalizedString("cancel", comment: "")
+    }
+    
+    struct TextFieldPrompt {
+        static let prompt = NSLocalizedString("start_typing_prompt", comment: "")
+    }
+    
+    struct AlertPrompt {
+        static let externalLink = NSLocalizedString("external_link_prompt", comment: "")
+    }
+    
+}
+
+
