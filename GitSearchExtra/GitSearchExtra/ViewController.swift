73bc87ef990c68a0fd607aa321144cd511ea338c
AleCheGH <chekushkinSasha@gmail.com>
2022-04-25 14:54:48 +0200

    initial project creation

---
+++ GitSearchExtra/GitSearchExtra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitSearchExtra
+//
+//  Created by Aliaksandr Chekushkin on 4/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
